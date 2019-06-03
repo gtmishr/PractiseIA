6275394abd50d0d2a300a51cca61a59992062eaa
William-Peterswald <43397871+William-Peterswald@users.noreply.github.com>
2019-06-03 12:46:34 +1000

    Command Line
    Command line and internet functions (which actually don't work, but we're working on it)

---
+++ commandline.java
@@ -0,0 +1,128 @@
+//package com.mkyong;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+//import javax.net.ssl.HttpsURLConnection;
+
+public class commandline {
+
+	public static void main(String[] args){
+
+    commandline program = new commandline();
+    System.out.println("Welcome!");
+    System.out.print("Enter a Function: ");
+    Scanner reader = new Scanner(System.in);
+    String a = reader.nextLine();
+		try {
+			URL url = new URL("https://ibcsamazonec2.tk/students");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+			con.setRequestMethod("GET");
+			Map<String, String> parameters = new HashMap<>();
+			parameters.put("param1", "val");
+
+			con.setDoOutput(true);
+
+			con.setRequestProperty("Content-Type", "application/json");
+			String contentType = con.getHeaderField("Content-Type");
+			String contentTypeTwo = con.getInputStream("Content-Type");
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer content = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+	    	content.append(inputLine);
+			}
+			System.out.println(contentType);
+			in.close();
+
+			con.disconnect();
+		} catch (Exception e){
+			System.out.print("HELLO ");
+			System.out.print(e);
+		}
+		if (a.equals("Reader") || a.equals("r")) {
+        program.Reader();
+    } else if (a.equals("Delete") || a.equals("d")) {
+        program.Delete();
+    }
+
+	}
+
+
+    private ArrayList < String > signedOffArrayList = new ArrayList < String > ();
+
+    public void Reader() {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String n = reader.nextLine();
+
+        if (n.length() == 10) {
+
+            //Checks to see if already marked off
+            if (!signedOffArrayList.contains(n)) {
+
+                // Insert code for collecting data from JSON file with additional details
+                signedOffArrayList.add(n);
+                System.out.println(n);
+                System.out.println(signedOffArrayList);
+                // Change to the name of the person once JSON file can be parsed
+                System.out.println(n + " has signed in.");
+
+
+            } else {
+
+                //If already signed in, inform user and delete
+                System.out.println(n + " has already signed in.");
+
+            }
+            System.out.print("Enter a Function: ");
+
+            String a = reader.nextLine();
+            if (a.equals("Reader")) {
+                Reader();
+            } else {
+                Delete();
+            }
+        }
+    }
+
+    public void Delete() {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String Deleted = reader.nextLine();
+
+        for (int i = 0; i < signedOffArrayList.size(); i++) {
+            if (signedOffArrayList.get(i).equals(Deleted)) {
+
+                System.out.println(Deleted + "Has been Removed");
+                signedOffArrayList.remove(i);
+                System.out.println(signedOffArrayList);
+
+            }
+        }
+
+        System.out.print("Enter a Function: ");
+        String a = reader.nextLine();
+
+        if (a.equals("Reader")) {
+            Reader();
+        } else {
+            Delete();
+        }
+    }
+}
